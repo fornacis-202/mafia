d836e92d6f255aef0143126a0a98aa01a29b914d
fornacis-202 <[fornacis.wro@gmail.com]>
2021-06-06 10:36:08 +0430

    updated Client

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -5,9 +5,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Client {
-    private Player player;
 
     public void start()  {
         String port;
@@ -19,6 +20,10 @@ public class Client {
             try (Socket socket = new Socket("127.0.0.1", Integer.parseInt(port))) {
                 ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
                 ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                ExecutorService executorService = Executors.newCachedThreadPool();
+                executorService.execute(new Sender(out,socket));
+                executorService.execute(new Receiver(in , socket));
+
             } catch (IOException | NumberFormatException e) {
                 System.out.println("can not connect to this server");
             }
